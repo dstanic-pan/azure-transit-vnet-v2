301fbdae37d744b657660e08cd0cfbffc464a752
Dejan Stanic <33906473+dstanic-pan@users.noreply.github.com>
2019-07-03 00:32:26 +0200

    Update main.tf

--- WebInDeploy/main.tf
+++ WebInDeploy/main.tf
@@ -663,70 +663,74 @@ resource "azurerm_virtual_machine" "spoke1-dev01" {
 }
 
 #  Outputs to the console
-output "" {
-  value = "= = =The firewalls may take up to 5 minutes to finish deploying.  You can proceed with additional steps once you can access the GUI= = = ="
-}
+#output "" {
+#  value = "= = =The firewalls may take up to 5 minutes to finish deploying.  You can proceed with additional steps once you can access the GUI= = = ="
+#}
 
-output "a) USERNAME    " {
-  value = "${var.username}"
-}
+#output "RESOURCE_GROUP_NAME" {
+#  value = "${azurerm_resource_group.rg.name}"
+#}
 
-output "b) PASSWORD    " {
-  value = "${var.password}"
-}
+#output "a) USERNAME    " {
+#  value = "${var.username}"
+#}
 
-output "c) PUBLIC IP FW1 MGMT " {
-  value = "${azurerm_public_ip.fw1nic0pip.ip_address}"
-}
+#output "b) PASSWORD    " {
+#  value = "${var.password}"
+#}
 
-output "d) PUBLIC IP FW2 MGMT " {
-  value = "${azurerm_public_ip.fw2nic0pip.ip_address}"
-}
+#output "c) PUBLIC IP FW1 MGMT " {
+#  value = "${azurerm_public_ip.fw1nic0pip.ip_address}"
+#}
 
-output "e) PUBLIC LB IP" {
-  value  = "${azurerm_public_ip.publiclbpip.ip_address}"
-}
+#output "d) PUBLIC IP FW2 MGMT " {
+#  value = "${azurerm_public_ip.fw2nic0pip.ip_address}"
+#}
 
-output "f) EGRESS LB IP" {
-  value  = "${var.egresslb_ip}"
-}
+#output "e) PUBLIC LB IP" {
+#  value  = "${azurerm_public_ip.publiclbpip.ip_address}"
+#}
 
-output "g) HUB VNET CIDR" {
-  value  = "${var.vnet_cidr}"
-}
+#output "f) EGRESS LB IP" {
+#  value  = "${var.egresslb_ip}"
+#}
 
-output "h) UNTRUST SUBNET CIDR" {
-  value  = "${var.subnet1_cidr}"
-}
+#output "g) HUB VNET CIDR" {
+#  value  = "${var.vnet_cidr}"
+#}
 
-output "i) TRUST SUBNET CIDR" {
-  value  = "${var.subnet2_cidr}"
-}
+#output "h) UNTRUST SUBNET CIDR" {
+#  value  = "${var.subnet1_cidr}"
+#}
 
-output "j) SPOKE1 VNET CIDR" {
-  value  = "${var.spoke1_vnet_cidr}"
-}
+#output "i) TRUST SUBNET CIDR" {
+#  value  = "${var.subnet2_cidr}"
+#}
 
-output "k) WEB SUBNET CIDR" {
-  value  = "${var.subnet10_cidr}"
-}
+#output "j) SPOKE1 VNET CIDR" {
+#  value  = "${var.spoke1_vnet_cidr}"
+#}
 
-output "l) APP SUBNET CIDR" {
-  value  = "${var.subnet11_cidr}"
-}
+#output "k) WEB SUBNET CIDR" {
+#  value  = "${var.subnet10_cidr}"
+#}
 
-output "m) DB SUBNET CIDR" {
-  value  = "${var.subnet12_cidr}"
-}
+#output "l) APP SUBNET CIDR" {
+#  value  = "${var.subnet11_cidr}"
+#}
 
-output "n) DEV SUBNET CIDR" {
-  value  = "${var.subnet13_cidr}"
-}
+#output "m) DB SUBNET CIDR" {
+#  value  = "${var.subnet12_cidr}"
+#}
 
-output "o) IP WEB1" {
-  value  = "${azurerm_network_interface.spoke1-web-nic0.private_ip_address}"
-}
+#output "n) DEV SUBNET CIDR" {
+#  value  = "${var.subnet13_cidr}"
+#}
 
-output "p) IP DEV" {
-  value  = "${azurerm_network_interface.spoke1-dev01-nic0.private_ip_address}"
-}
+#output "o) IP WEB1" {
+#  value  = "${azurerm_network_interface.spoke1-web-nic0.private_ip_address}"
+#}
+
+#output "p) IP DEV" {
+#  value  = "${azurerm_network_interface.spoke1-dev01-nic0.private_ip_address}"
+#}
